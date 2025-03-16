c34b3e588dbebd04e13baa285bf67b4cc5954af5
Luis Santana <luissantananet@gmail.com>
2025-03-16 14:13:46 -0300

    adiciona configuração do caminho de importação no teste de unidade para a classe Database

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'utils')))
 from src.utils.database import Database
 import mysql.connector
 import mysql.connector.errors
