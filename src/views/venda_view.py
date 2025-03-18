65ee95a26ab17f0c5724603a5b35db85189c3634
Luis Santana <luissantananet@gmail.com>
2025-03-18 16:52:13 -0300

    refatora a view de vendas para usar caminho dinâmico para o arquivo UI

--- src/views/venda_view.py
+++ src/views/venda_view.py
@@ -1,9 +1,11 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
+import os
 
 class CaixaView(QMainWindow):
     def __init__(self, parent=None):
         super(CaixaView, self).__init__(parent)
-        uic.loadUi("screens/frm_venda.ui", self)
+        ui_path = os.path.join(os.path.dirname(__file__), 'screens', 'frm_venda.ui')
+        uic.loadUi(ui_path, self)
         self.setWindowTitle("Venda")
         self.setGeometry(100, 100, 800, 600)
