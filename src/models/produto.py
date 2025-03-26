c4aac1664c683e24727240347ee35a9cd394882f
Luis Santana <luissantananet@gmail.com>
2025-03-26 20:22:51 -0300

    adiciona métodos para cadastro, atualização, exclusão e busca de produtos na classe Produto

--- src/models/produto.py
+++ src/models/produto.py
@@ -1,3 +1,5 @@
+from utils.database import Database
+
 class Produto:
     def __init__(self, ean, descricao,Grupo, fabricante,unidade,precocomp,precovenda, markup):
         self.ean = ean
@@ -14,14 +16,28 @@ class Produto:
             raise ValueError("O preço de custo não pode ser zero para calcular o markup.")
     def cadastro_produto(self):
         # Aqui você pode adicionar a lógica para cadastrar o produto no banco de dados ou em outro lugar
-        pass
+        try:
+            query = "INSERT INTO tblproduto (ean, descricao, categoria, fabricante, unidade, precocusto, markup, precovenda) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+            values = (self.ean, self.Descricao, self.Grupo, self.fabricante, self.unidade, self.precocomp, self.markup, self.precovenda)
+            # Execute a consulta no banco de dados aqui
+            db = Database()
+            return db.execute(query, values)
+        except Exception as e:
+            print(f"Erro ao cadastrar produto: {e}")
+            return False
+
     def atualizar_produto(self):
         # Aqui você pode adicionar a lógica para atualizar o produto no banco de dados ou em outro lugar
-        pass
+        db = Database()
+        return db.execute("UPDATE tblproduto SET ean = %s, descricao = %s, categoria = %s, fabricante = %s, unidade = %s, precocusto = %s, markup = %s, precovenda = %s WHERE ean = %s",
+                 (self.ean, self.Descricao, self.Grupo, self.fabricante, self.unidade, self.precocomp, self.markup, self.precovenda, self.ean))
     def excluir_produto(self):
         # Aqui você pode adicionar a lógica para excluir o produto do banco de dados ou em outro lugar
-        pass
+        db = Database()
+        return db.delete_id("tblproduto", "ean", self.ean)
+
     def buscar_produto(self):
         # Aqui você pode adicionar a lógica para buscar o produto no banco de dados ou em outro lugar
-        pass
+        db = Database()
+        return db.select_id("tblproduto", "ean", self.ean) 
 
